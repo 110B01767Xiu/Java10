ee00f60f2a6b7ac744914a364780c57746dcbbd4
110B01767Xiu <115057022+110B01767Xiu@users.noreply.github.com>
2022-10-31 05:00:19 +0800

    Create Class09.java

---
+++ Class09.java
@@ -0,0 +1,16 @@
+class Caaa {
+	public void display() {
+		System.out.println("printed from Caaa class");
+	}
+}
+class Cbbb extends Caaa {
+	public void display() {
+		System.out.println("printed from Cbbb class");
+	}
+}
+public class Main {
+	public static void main(String args[]) {
+		Caaa bb = new Cbbb();
+		bb.display();
+	}
+}
